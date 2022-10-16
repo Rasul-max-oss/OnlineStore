4359bdf54bf3e646851505cda1a1b6876e84a611
Rasul <76030734+Rasul-max-oss@users.noreply.github.com>
2022-10-16 20:23:08 +0800

    s

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { useEffect } from 'react';
 import './App.css';
+import { useTelegram } from './hooks/useTelegram';
 
 function App() {
+	const { onToggleButton, tg } = useTelegram();
+	useEffect(() => {
+		tg.ready();
+	}, [])
+
 
 	return (
 		<div className="App">
-
+			<button onClick={onToggleButton}>toggle</button>
 		</div>
 	);
 }
